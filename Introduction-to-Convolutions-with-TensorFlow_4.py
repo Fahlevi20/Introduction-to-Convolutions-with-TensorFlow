05b65c07761f24f5d71c45260bcc496d0d0ef763
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-21 19:32:46 +0700

    Create Introduction-to-Convolutions-with-TensorFlow_4.py

---
+++ Introduction-to-Convolutions-with-TensorFlow_4.py
@@ -0,0 +1,18 @@
+for x in range(1,size_x-1):
+  for y in range(1,size_y-1):
+      convolution = 0.0
+      convolution = convolution + (i[x - 1, y-1] * filter[0][0])
+      convolution = convolution + (i[x, y-1] * filter[0][1])
+      convolution = convolution + (i[x + 1, y-1] * filter[0][2])
+      convolution = convolution + (i[x-1, y] * filter[1][0])
+      convolution = convolution + (i[x, y] * filter[1][1])
+      convolution = convolution + (i[x+1, y] * filter[1][2])
+      convolution = convolution + (i[x-1, y+1] * filter[2][0])
+      convolution = convolution + (i[x, y+1] * filter[2][1])
+      convolution = convolution + (i[x+1, y+1] * filter[2][2])
+      convolution = convolution * weight
+      if(convolution<0):
+        convolution=0
+      if(convolution>255):
+        convolution=255
+      i_transformed[x, y] = convolution
